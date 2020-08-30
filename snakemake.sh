4f491271b89924b5f51a2fea736516cadac47339
Christopher Baker <cbaker@holy7c26602.rc.fas.harvard.edu>
2020-08-29 23:33:56 -0400

    update barcodes folder

--- snakemake.sh
+++ snakemake.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #SBATCH -N 1    # nodes
-#SBATCH -n 4    # CPUs
-#SBATCH --mem-per-cpu=16G    # memory
+#SBATCH -n 8    # CPUs
+#SBATCH --mem-per-cpu=4G    # memory
 #SBATCH -p pierce    # partition
 #SBATCH -t 0-06:00    # runtime
 #SBATCH -o snakemake.out    # output file
 #SBATCH -e snakemake.err    # error file
 
-module load gcc/7.1.0-fasrc01 stacks/2.4-fasrc01
-
-snakemake -j 4
+snakemake -j 8 --use-conda flagstat_CM_CN_TP
