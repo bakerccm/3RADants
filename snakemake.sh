7ae2a587654cdf5de36f67c644fb4311a0441829
Christopher Baker <cbaker@boslogin03.rc.fas.harvard.edu>
2021-03-05 12:52:00 -0500

    update sbatch parameters for mapping to genome

--- snakemake.sh
+++ snakemake.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -N 1    # nodes
-#SBATCH -n 5    # CPUs
-#SBATCH --mem-per-cpu=32G    # memory
+#SBATCH -n 40    # CPUs
+#SBATCH --mem=10G    # memory
 #SBATCH -p shared    # partition
-#SBATCH -t 0-06:00    # runtime
+#SBATCH -t 0-09:00    # runtime
 #SBATCH -o snakemake.out    # output file
 #SBATCH -e snakemake.err    # error file
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=bakerccm@gmail.com
 
-snakemake -j 5 dereplicate_all
+snakemake -j 40 --use-conda all_mapped_sample_stats
