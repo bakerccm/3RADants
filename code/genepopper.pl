f92ab76aacd7dab5f717f054aca5cdedaa9434d1
Chris Baker <bakerccm@gmail.com>
2021-03-24 22:13:06 -0400

    write output to log file instead of stdout

--- code/genepopper.pl
+++ code/genepopper.pl
@@ -95,7 +95,7 @@ foreach (keys %data_hash) {
   $missing_data += $data_hash{$_}
 }
 my $print_this = 1 - $missing_data / $individuals;
-print STDOUT "The total number of SNPs is $length.
+print OUTPUT_LOG "The total number of SNPs is $length.
 The total number of individuals is $individuals.
 The total proportion of missing data is $print_this\n";
 foreach my $proportion (.1, .2, .5, .8, .9, 1) {
